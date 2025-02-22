45478bb58b0925cc55f793ead0f6c7538a2ea3f0
Marsh2006 <91168301+Marsh2006@users.noreply.github.com>
2025-02-21 17:23:15 -0800

    Added descriptions

--- Market_game.py
+++ Market_game.py
@@ -1,10 +1,22 @@
 import numpy as np
 
+# One player is the maker and the other is the taker.
+# They agree on a width beforehand, such as 10.
+# The taker generates a random integer between 1 and 100 inclusive (so each number has a 1% chance of appearing).
+# This number is the true value of the contract. The taker sees the true value; the maker does not.
+
+# There are ten rounds of action.
+# In each round, the maker makes a market on the contract with the agreed upon width.
+# For example, in the first round, the maker could say "45@55." The taker must trade one contract on this market.
+# On the example market, the taker can buy for 55 ("take"), or sell at 45 ("sold").
+
+
+
 class Market_Game:
     fair_values = np.arange(1, 101)
     widths = np.arange(0, 100, 2)
     
-    def sim_round(radius, fair_value, left, right):
+    def simulate_round(radius, fair_value, left, right):
         mid = left + (right - left) / 2
         if mid < fair_value: # Taker buys
             left = mid + 1
@@ -15,7 +27,8 @@ class Market_Game:
 
                 
         
-    
+        
+    # Test cumulative expected value at 100 fair values for 50 market widths
     for width in widths:
         radius = width / 2
         total_PnL = np.array([0,0])
@@ -24,9 +37,10 @@ class Market_Game:
             left = 1
             right = 100
             for i in range(10):
-                profits = profits + sim_round(radius, fair_value, left, right)
+                profits = profits + simulate_round(radius, fair_value, left, right)
             total_PnL = total_PnL + profits
         print("Width: " + str(width) + " Total PnL: " + str(total_PnL))
 
+# Using this simulation, we find that width = 50 give no edge to either player (for 10 rounds)
 
     
